517709db52f2c4eea8b523d33338d7272df4aa5a
samkosal <67755908+samkosal@users.noreply.github.com>
2026-01-13 14:48:33 -0800

    finished

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,22 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        Node head = new Node('g', null);
+        Node ryan = new Node('s', null);
+        head.next = ryan;
+        ryan.next = new Node('t', null);
+        ryan.next.next = new Node('x', null);
+        
+        printList(head);
+
+        
+    }
+
+    public static void printList(Node start) {
+        Node current = start;
+
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
     }
 }
